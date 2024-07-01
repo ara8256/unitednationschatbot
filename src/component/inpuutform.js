3d225fcd86d1983db4c3a7f7db0b0dd8b2968eb5
ara8256 <arehman.bscs21seecs@seecs.edu.pk>
2024-07-02 00:28:41 +0500

    commit

--- src/component/inpuutform.js
+++ src/component/inpuutform.js
@@ -1,25 +1,28 @@
-
 import React from 'react';
-import "../css/input.css"
+import "../css/input.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 const ChatInput = () => {
     return (
-        <div className="d-flex align-items-center justify-content-center mx-auto mt-3" style={{ maxWidth: "25%", borderRadius: "5vh", outlineStyle: "solid" }}>
-        <div style={{ flexGrow: 1 }}>
-            <input
-                type="text"
-                className="form-control border-0"
-                placeholder="Message"
-                style={{ background: "none", border: "none", width: "100%", fontSize: "2vh", outline: "none" }}
-            />
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-sm-10 col-md-8 col-lg-6 mt-3">
+                    <div className="d-flex align-items-center justify-content-between" style={{ borderRadius: "5vh", border: "1px solid #ccc", padding: "0.5rem"}}>
+                        <input
+                            type="text"
+                            className="form-control flex-grow-1 no-outline"
+                            placeholder="Message"
+                            style={{ background: "none", border: "none", fontSize: "1rem" }}
+                        />
+                        <button className="send-button btn btn-link p-0 ml-2">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2 21L23 12L2 3V10L17 12L2 14V21Z" fill="currentColor"/>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div>
-            <button className="send-button" style={{ background: "none", border: "none", cursor: "pointer" }}>
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M2 21L23 12L2 3V10L17 12L2 14V21Z" fill="currentColor"/>
-                </svg>
-            </button>
-        </div>
-    </div>
     );
 };
 
